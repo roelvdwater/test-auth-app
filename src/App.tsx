dd69639b18b17d4368b0635ea410b5733733021d
Roel van de Water <3884615+roelvdwater@users.noreply.github.com>
2022-08-02 08:38:50 +0200

    Add sample PKCE code

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useAuth } from "oidc-react";
 
 function App() {
+  const auth = useAuth();
+
+  if (auth.userData === null) {
+    return <></>;
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <p>You are signed in, {auth.userData?.profile.email}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { AuthProvider, AuthProviderProps } from "oidc-react";
+
+const oidcConfig: AuthProviderProps = {
+  onSignIn: () => {
+    window.location.replace(window.location.toString().split(/[?#]/)[0]); // Remove authentication query part
+  },
+  authority: "https://my-fake-authority.com/",
+  clientId: "my-client-id",
+  redirectUri: window.location.toString(),
+  scope: "openid profile",
+};
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider {...oidcConfig}>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
 
